21fa6ab48d9690084b304554b820871bb438dc2e
EKil <loreleyko@gmail.com>
2024-10-09 00:54:41 +0200

    First commit

---
+++ src/ait/chat/client/ChatClientAppl.java
@@ -0,0 +1,34 @@
+package ait.chat.client;
+
+import ait.chat.client.task.MessageReceiver;
+import ait.chat.client.task.MessageSender;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatClientAppl {
+    public static void main(String[] args) {
+        String serverHost = "127.0.0.1"; //localhost
+        int port = 9090;
+
+        try (Socket socket = new Socket(serverHost, port)) {
+            Thread receiver = new Thread(new MessageReceiver(socket));
+            Thread sender = new Thread(new MessageSender(socket));
+
+            receiver.start();
+            sender.start();
+
+            sender.join();
+            receiver.join();
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/ait/chat/client/task/MessageReceiver.java
@@ -0,0 +1,26 @@
+package ait.chat.client.task;
+
+import java.io.*;
+import java.net.Socket;
+
+public class MessageReceiver implements Runnable {
+    private Socket socket;
+
+    public MessageReceiver(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                String message = socketReader.readLine();
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Connection is closed");
+        }
+    }
+}
